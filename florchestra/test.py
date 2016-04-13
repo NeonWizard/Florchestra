440c18b8ce5761a31434d366d85d98d9796d1c70
NeonWizard <thelordofgiggles@gmail.com>
2016-04-13 01:08:59 +0000

    removed floppy-music software because it sucked

--- florchestra/test.py
+++ florchestra/test.py
@@ -4,34 +4,39 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 GPIO.setup(18, GPIO.OUT) # Step (Floppy Pin 20)
 GPIO.setup(23, GPIO.OUT) # Direction (Floppy Pin 18)
 GPIO.setup(24, GPIO.OUT) # Write (Floppy Pin 22)
 
 print("Turning on")
+print("Press ctrl+c to terminate")
 
-GPIO.output(23, GPIO.HIGH) # Go one direction
-time.sleep(.005)
-for num in range(1, 75):
-    GPIO.output(18, GPIO.HIGH) # Step
-    time.sleep(.004)
-    GPIO.output(24, GPIO.HIGH) # Write
-    time.sleep(.004)
-    GPIO.output(18, GPIO.LOW)
-    GPIO.output(24, GPIO.LOW)
+while True:
 
-GPIO.output(23, GPIO.LOW) # Go the opposite direction
-time.sleep(.005)
-for num in range(1, 75):
-    GPIO.output(18, GPIO.HIGH) # Step
-    time.sleep(.004)
-    GPIO.output(24, GPIO.HIGH) # Write
-    time.sleep(.004)
-    GPIO.output(18, GPIO.LOW)
-    GPIO.output(24, GPIO.LOW)
+	GPIO.output(23, GPIO.HIGH) # Go one direction
+	time.sleep(.005)
+	for num in range(1, 75):
+	    GPIO.output(18, GPIO.HIGH) # Step
+	    time.sleep(.004)
+	    GPIO.output(24, GPIO.HIGH) # Write
+	    time.sleep(.004)
+	    GPIO.output(18, GPIO.LOW)
+	    GPIO.output(24, GPIO.LOW)
 
-time.sleep(1)
+
+	GPIO.output(23, GPIO.LOW) # Go the opposite direction
+	time.sleep(.005)
+	for num in range(1, 75):
+	    GPIO.output(18, GPIO.HIGH) # Step
+	    time.sleep(.004)
+	    GPIO.output(24, GPIO.HIGH) # Write
+	    time.sleep(.004)
+	    GPIO.output(18, GPIO.LOW)
+	    GPIO.output(24, GPIO.LOW)
+
+	time.sleep(.005)
 
 print("Exiting")
 
