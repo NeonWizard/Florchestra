cdacfe619aa3a06c0883d572410434b047478140
NeonWizard <averagewizard13@gmail.com>
2016-06-18 23:07:09 -0600

    Single frive midikeyboard support

--- conductor/python/midikeyboard.py
+++ conductor/python/midikeyboard.py
@@ -1,15 +1,16 @@
 import mido
-
 mido.set_backend("mido.backends.pygame")
 
-# print mido.get_output_names()
+import serialcomm
 
+# print mido.get_output_names()
 # out = mido.open_output("Microsoft MIDI Mapper")
 
 def handleRaw(msg):
 	state = msg.type=="note_on" and msg.velocity > 0
-	note = msg.note
+	note = msg.note-46
 	print str(note) + " - " + str(state)
+	serialcomm.sendNote(note, 0)
 
 with mido.open_input("USB2.0-MIDI") as inp:
 	for message in inp:
