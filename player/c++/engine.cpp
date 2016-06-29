e3c4bb8d19cd5403dd879eaebb377f2fbefb85d2
NeonWizard <averagewizard13@gmail.com>
2016-06-29 02:53:50 +0000

    Added support for fourth frive

--- player/c++/engine.cpp
+++ player/c++/engine.cpp
@@ -31,24 +31,25 @@ typedef uint8_t byte;
 //  Individual frive information
 // ------------------------------
 // The array positions are numbered based on position in the Frive array
-byte pins[3][3] = {
+byte pins[4][3] = {
 	// Dir, step, led
 	{17, 18, 19},
 	{13, 26, 6},
-	{23, 24, 25}
+	{23, 24, 25},
+	{20, 21, 22}
 };
-byte friveCount = 3;
+byte friveCount = 4;
 
 // 3.5" frives have 80 tracks and 5.25" have 50
 // Subtract 8 to add a bit of padding
 // Double it because of toggling
-byte MAX_POSITIONS[]    = {144, 144, 144};
-byte currentPositions[] = {0, 0, 0};
-byte currentDirection[] = {0, 0, 0};
-bool currentVoltage[]   = {0, 0, 0};
+byte MAX_POSITIONS[]    = {144, 144, 144, 144};
+byte currentPositions[] = {0, 0, 0, 0};
+byte currentDirection[] = {0, 0, 0, 0};
+bool currentVoltage[]   = {0, 0, 0, 0};
 
-unsigned int currentPeriod[3] = {0, 0, 0}; // Current period is how long until another step
-unsigned int currentTick[3]   = {0, 0, 0}; // Counts how long has passed since the frive has been stepped
+unsigned int currentPeriod[] = {0, 0, 0, 0}; // Current period is how long until another step
+unsigned int currentTick[]   = {0, 0, 0, 0}; // Counts how long has passed since the frive has been stepped
 
 static bool stepmethod;
 
@@ -76,7 +77,6 @@ int setup()
 }
 
 void stepFrive_oscillating(byte frive)
-// Frive is a number that counts up from 0 based on what position the pins are in the frive array
 {
 	currentDirection[frive] = currentVoltage[frive] ? !currentDirection[frive] : currentDirection[frive];
 	digitalWrite(pins[frive][0], currentDirection[frive]);
@@ -86,7 +86,6 @@ void stepFrive_oscillating(byte frive)
 }
 
 void stepFrive_sliding(byte frive)
-// Frive is a number that counts up from 0 based on what position the pins are in the frive array
 {
 	if (currentPositions[frive] >= MAX_POSITIONS[frive])
 	{
@@ -150,7 +149,16 @@ void tick()
 			STEPFRIVEF(2);
 			currentTick[2]=0;
 		}
-	}	
+	}
+	if (currentPeriod[3]>0)
+	{
+		currentTick[3] += delta;
+		if (currentTick[3] >= currentPeriod[3])
+		{
+			STEPFRIVEF(3);
+			currentTick[3]=0;
+		}
+	}
 	delayMicroseconds(5); // Prevent the loop from going too fast and giving itself a bad time
 }
 
