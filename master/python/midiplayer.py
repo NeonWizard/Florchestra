26c81d69fcb3b437758325424874bf554740354c
NeonWizard <averagewizard13@gmail.com>
2016-07-14 19:48:18 +0000

    Added data files for songs

--- master/python/midiplayer.py
+++ master/python/midiplayer.py
@@ -4,29 +4,24 @@ mido.set_backend("mido.backends.pygame")
 
 import serialcomm
 import midiportnames
+import playertools
 
-def getChannels(songfile):
-	channels = []
-	for message in mido.MidiFile(song):
-		if isinstance(message, mido.MetaMessage): # Filter out meta messages
-			continue
-
-		if message.channel not in channels:
-			channels.append(message.channel)
-
-	return channels
-
-def handleRaw(msg, tracklist):
+def handleRaw(msg, sd): # sd = song data
 	if isinstance(msg, mido.MetaMessage) or not hasattr(msg, "note"): # Filter out meta messages
 		return
 
 	# Get the channel it's on and convert to floppy track
-	if msg.channel not in tracklist: return
-	track = tracklist.index(msg.channel)
+	if msg.channel not in sd["TRACK_KEY"]: return
+	track = sd["TRACK_KEY"].index(msg.channel)
+
 	# Determine if it's a NOTE_ON or NOTE_OFF
 	state = msg.type=="note_on" and msg.velocity > 0
-	# Convert the message to a floppy note
-	note = msg.note-46+octavemod*12
+
+	# Convert the message to a floppy note then transpose
+	note = msg.note-46
+	note += sd["TRANSPOSE"] # Transposition
+
+	# Capping on either side of the range
 	if note > 31:
 		note = 20+((note-32)%12)
 	if note <= 0:
@@ -37,7 +32,9 @@ def handleRaw(msg, tracklist):
 	# If it isn't playable by the florchestra
 	#if note <= 0 or note > 31: return
 	if note <= 0: return
-	if note > 31: print note
+	if note > 31:
+		print note
+		return
 
 	if state:
 		serialcomm.sendNote(note, track)
@@ -51,35 +48,14 @@ if len(sys.argv) != 2:
 	print "Invalid argument count!"
 	sys.exit()
 
-song ="../midis/"+sys.argv[1]+".mid"
+print "Getting song info..."
+songdata = playertools.readSongData("../songs/"+sys.argv[1])
+print "Reading MIDI file into memory..."
+songfile = mido.MidiFile("../songs/"+songdata["MIDI_NAME"]+".mid")
 
-print "Getting track list..."
-#tracklist = getChannels(song)
-
-# Mayhem - The Sound
-#tracklist = [-1, 0, 2, 4]
-#tracklist = [-1, 2, 4, 0]
-#octavemod = 2
-
-# Eurythmics - Sweet dreams
-#tracklist = [-1, 6, 2, 5]
-#octavemod = 2
-
-# TLT - FNAF
-#tracklist = [0, 3, 4, 6]
-#octavemod = 2
-
-# Glaze - Beyond
-#tracklist = [-1, 3, 4, 2]
-#octavemod = 3
-
-# Laroux - Bulletproof
-tracklist = [-1, 3, 1, 2]
-octavemod = 3
-
-print tracklist
 print "Ready to begin."
-for message in mido.MidiFile(song).play():
-	handleRaw(message, tracklist)
+
+for message in songfile.play():
+	handleRaw(message, songdata)
 	#out.send(message)
 out.close()
---
+++ master/python/playertools.py
@@ -0,0 +1,26 @@
+def getChannels(songfile):
+	channels = []
+	for message in songfile:
+		if isinstance(message, mido.MetaMessage): # Filter out meta messages
+			continue
+
+		if message.channel not in channels:
+			channels.append(message.channel)
+
+	return channels
+
+def readSongData(name):
+	songdata = {}
+
+	with open(name+".dat", 'r') as openFile:
+		for line in openFile:
+			line = line.rstrip("\n").rstrip("\r")
+			data = line.split(":")
+			songdata[data[0]] = data[1][1:] # [1:] is to remove the space in the beginning
+
+	# Cleaning up the raw data
+	songdata["TRANSPOSE"] = int(songdata["TRANSPOSE"])
+	songdata["TRACK_KEY"] = [int(i) for i in songdata["TRACK_KEY"].replace(" ", "").split(",")]
+
+	print songdata
+	return songdata
