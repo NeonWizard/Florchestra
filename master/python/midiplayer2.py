0f055dd0eafa7c1759146fde4c16d7d123b4a0b7
NeonWizard <averagewizard13@gmail.com>
2016-10-10 18:22:30 +0000

    Added master support for two byte communication

---
+++ master/python/midiplayer2.py
@@ -0,0 +1,61 @@
+import sys
+import mido
+mido.set_backend("mido.backends.pygame")
+
+import serialcomm
+import midiportnames
+import playertools
+
+def handleRaw(msg, sd): # sd = song data
+	if isinstance(msg, mido.MetaMessage) or not hasattr(msg, "note"): # Filter out meta messages
+		return
+
+	# Get the channel it's on and convert to floppy track
+	if msg.channel not in sd["TRACK_KEY"]: return
+	track = sd["TRACK_KEY"].index(msg.channel)
+
+	# Determine if it's a NOTE_ON or NOTE_OFF
+	state = msg.type=="note_on" and msg.velocity > 0
+
+	# Convert the message to a floppy note then transpose
+	note = msg.note-55
+	note += sd["TRANSPOSE"] # Transposition
+
+	# Capping on either side of the range
+	if note > 63:
+		note = 52+((note-64)%12)
+	if note <= 0:
+		# Fix this later
+		print "Jaspar"
+		note = abs(note)%12
+
+	# If it isn't playable by the florchestra
+	#if note <= 0 or note > 31: return
+	if note <= 0: return
+	if note > 63:
+		print note
+		return
+
+	if state:
+		serialcomm.sendChars(note, track)
+	else:
+		serialcomm.sendChars(0, track)
+
+outPort = list(set(mido.get_output_names())&set(midiportnames.outs))[0]
+out = mido.open_output(outPort)
+
+if len(sys.argv) != 2:
+	print "Invalid argument count!"
+	sys.exit()
+
+print "Getting song info..."
+songdata = playertools.readSongData("../songs/"+sys.argv[1])
+print "Reading MIDI file into memory..."
+songfile = mido.MidiFile("../songs/"+songdata["MIDI_NAME"]+".mid")
+
+print "Ready to begin."
+
+for message in songfile.play():
+	handleRaw(message, songdata)
+	#out.send(message)
+out.close()
