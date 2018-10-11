ed37c69943192d6935c52209048a7736e57bfcf1
NeonWizard <averagewizard13@gmail.com>
2018-10-11 06:42:31 +0000

    Made new midiplayers that separate notes into different tracks based on availability

---
+++ master/python/midiplayer3.py
@@ -0,0 +1,75 @@
+import sys
+import mido
+mido.set_backend("mido.backends.pygame")
+
+import serialcomm
+import playertools
+
+class Handler:
+	def __init__(self):
+		self.frives = [0, 0, 0, 0, 0, 0]
+
+		self.resetFrives()
+	
+	def resetFrives(self):
+		for i in range(len(self.frives)):
+			serialcomm.sendNote(0, i)
+			self.frives[i] = 0
+			
+
+	def sortAndSend(self):
+		self.frives = sorted(self.frives, reverse=True)
+		for i in range(len(self.frives)):
+			serialcomm.sendNote(self.frives[i], i)
+
+	def parseNote(self, msg, sd):
+		if isinstance(msg, mido.MetaMessage) or not hasattr(msg, "note"): # Filter out meta messages
+			return
+
+		note = msg.note - 46
+		note += sd["TRANSPOSE"]
+
+		# Capping on either side of the range
+		if note > 31:
+			note = 20+((note-32)%12)
+		if note <= 0:
+			# Fix this later
+			print "Jaspar"
+			note = abs(note)%12
+
+		if msg.type == "note_on" and msg.velocity > 0:
+			self.playNote(note)
+		else:
+			self.stopNote(note)
+
+	def playNote(self, note):
+		for i in range(len(self.frives)):
+			if self.frives[i]: continue # If this frive is already playing a note
+
+			self.frives[i] = note # Document the note playing
+			self.sortAndSend()
+			return
+
+	def stopNote(self, note):
+		for i in range(len(self.frives)):
+			if self.frives[i] == note:
+				self.frives[i] = 0
+				serialcomm.sendNote(0, i)
+				# Putting a return statement here could reduce lag but also might help clear out bugged notes
+
+
+if len(sys.argv) != 2:
+	print "Invalid argument count!"
+	sys.exit()
+
+print "Getting song info..."
+songdata = playertools.readSongData("../songs/"+sys.argv[1])
+print "Reading MIDI file into memory..."
+songfile = mido.MidiFile("../songs/"+songdata["MIDI_NAME"]+".mid")
+
+h = Handler()
+
+print "Ready to begin."
+
+for message in songfile.play():
+	h.parseNote(message, songdata)
